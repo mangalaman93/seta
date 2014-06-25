a73dcac8bc51104ebc6c3290baf62d98cbfe0dd7
Aman Mangal <mangalaman93@gmail.com>
2014-06-26 00:17:43 +0530

    Add example tests for dequeue(incomplete)

--- test/dequeue_test.c
+++ test/dequeue_test.c
@@ -1,22 +1,55 @@
 #include "minunit.h"
 #include "dequeue.h"
 
-dequeue_t *list;
+dequeue_t *dq;
 
 void test_setup() {
-	list = dequeue_create();
+	dq = dequeue_create();
 }
 
 void test_teardown() {
-	free(list);
+	dequeue_destroy(dq);
 }
 
-MU_TEST(test_check) {
+MU_TEST(test_basic) {
+	mu_assert(!dequeue_is_not_empty(dq), "list should be empty!");
+	mu_assert_int_eq(0, dequeue_add_head(dq, 1));
+	mu_assert_int_eq(1, dequeue_size(*dq));
+	mu_assert_int_eq(1, dequeue_get_element(*dq, 0));
+	mu_assert(!dequeue_get_element(*dq, 1), "should return NULL");
+
+	mu_assert_int_eq(0, dequeue_add_head(dq, 2));
+	mu_assert_int_eq(2, dequeue_size(*dq));
+
+	mu_assert_int_eq(0, dequeue_add_head(dq, 3));
+	mu_assert_int_eq(3, dequeue_size(*dq));
+
+	mu_assert_int_eq(0, dequeue_add_head(dq, 4));
+	mu_assert_int_eq(4, dequeue_size(*dq));
+
+	mu_assert_int_eq(0, dequeue_add_head(dq, 5));
+	mu_assert_int_eq(5, dequeue_size(*dq));
+	mu_assert_int_eq(2, dequeue_get_element(*dq, 3));
+	mu_assert(dequeue_is_not_empty(dq), "list should not be empty!");
+
+	mu_assert_int_eq(5, dequeue_extract_head(dq));
+
+	mu_assert_int_eq(4, dequeue_extract_head(dq));
+	mu_assert_int_eq(3, dequeue_size(*dq));
+	mu_assert_int_eq(1, dequeue_extract_tail(dq));
+	mu_assert_int_eq(2, dequeue_size(*dq));
+
+	mu_assert_int_eq(3, dequeue_extract_head(dq));
+	mu_assert_int_eq(1, dequeue_size(*dq));
+
+	mu_assert_int_eq(2, dequeue_extract_head(dq));
+	mu_assert_int_eq(0, dequeue_size(*dq));
+	mu_assert(!dequeue_is_not_empty(dq), "list should be empty!");
 }
 
 MU_TEST_SUITE(test_suite) {
 	MU_SUITE_CONFIGURE(&test_setup, &test_teardown);
-	MU_RUN_TEST(test_check);
+	MU_RUN_TEST(test_basic);
 }
 
 int main(int argc, char *argv[]) {
