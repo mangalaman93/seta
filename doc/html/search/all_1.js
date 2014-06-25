5596f4526484d715a0cf8cdf894275756134d8e7
Luca Favatella <luca.favatella@erlang-solutions.com>
2014-06-25 14:08:59 +0100

    Delete HTML doc as it can be generated using 'make doc'
    The doc was deleted running 'make docclean'.

--- doc/html/search/all_1.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['main_20page',['Main Page',['../index.html',1,'']]],
-  ['mainpage_2edox',['mainpage.dox',['../mainpage_8dox.html',1,'']]]
-];
--- doc/html/search/all_2.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['theory_20on_20seta',['Theory on Seta',['../theory.html',1,'']]],
-  ['theory_2edox',['theory.dox',['../theory_8dox.html',1,'']]]
-];
